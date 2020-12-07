dbd9850904648298995d7156d7c0811957d9087b
Eric Lindeberg <ericlindeberg@gmail.com>
2020-12-07 17:04:42 +0100

    Day 07

---
+++ AoC2020/Days/Day07/Day07.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Days.Day07
+{
+    public class Day07 : BaseDay
+    {
+        private Dictionary<string, HashSet<string>> _bags;
+
+        public override string SolvePartOne()
+        {
+            _bags = Input
+                .Select(line => line.Substring(0, line.Length - 1).Replace(" bags", "").Replace(" bag", ""))
+                .ToDictionary(key => key.Split(" contain ")[0], val => val.Split(" contain ")[1].Split(", ").Select(c => c.Substring(2)).ToHashSet());
+
+            var total = 0;
+            foreach (var key in _bags.Keys)
+            {
+                if (HasShinyGold(key))
+                    total++;
+            }
+
+            return total.ToString();
+        }
+
+        bool HasShinyGold(string str)
+        {
+            if (_bags[str].Contains("shiny gold"))
+                return true;
+            foreach (var hs in _bags[str])
+            {
+                if (hs == " other") continue;
+                if (!HasShinyGold(hs))
+                    continue;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string SolvePartTwo()
+        {
+            _bags = Input
+                .Select(line => line[..^1].Replace(" bags", "").Replace(" bag", ""))
+                .ToDictionary(key => key.Split(" contain ")[0], val => val.Split(" contain ")[1].Split(", ").ToHashSet());
+
+            return CalculatePartTwo("shiny gold").ToString();
+        }
+
+        int CalculatePartTwo(string str)
+        {
+            var total = 0;
+            foreach (var val in _bags[str])
+            {
+                if (val != "no other")
+                {
+                    var amount = int.Parse(val.Substring(0, 1));
+                    total += amount + amount * CalculatePartTwo(val.Substring(2));
+                }
+                else
+                    break;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ AoC2020Benchmark/Day07Benchmark.cs
@@ -0,0 +1,16 @@
+﻿using AoC2020.Days.Day07;
+using BenchmarkDotNet.Attributes;
+
+namespace AoC2020Benchmark
+{
+    public class Day07Benchmark : BaseDayBenchmark
+    {
+        readonly Day07 _problem = new Day07();
+
+        [Benchmark]
+        public override string Part1() => _problem.SolvePartOne();
+
+        [Benchmark]
+        public override string Part2() => _problem.SolvePartTwo();
+    }
+}
