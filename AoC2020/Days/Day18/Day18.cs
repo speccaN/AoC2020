b270dce3d8097061c3bb81945354ea9d38891d57
Eric Lindeberg <ericlindeberg@gmail.com>
2020-12-19 14:19:45 +0100

    Day 18

---
+++ AoC2020/Days/Day18/Day18.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Days.Day18
+{
+    public class Day18 : BaseDay
+    {
+        private readonly Stack<Operation> _ops = new();
+        public override string SolvePartOne()
+        {
+            var total = 0L;
+            foreach (var line in Input)
+            {
+                _ops.Push(new Operation());
+
+                foreach (var chr in line.Replace(" ", ""))
+                {
+                    switch (chr)
+                    {
+                        case '+':
+                            _ops.Peek().Operators.Add('+');
+                            break;
+                        case '-':
+                            _ops.Peek().Operators.Add('-');
+                            break;
+                        case '*':
+                            _ops.Peek().Operators.Add('*');
+                            break;
+                        case '(':
+                            _ops.Push(new Operation());
+                            break;
+                        case ')':
+                            var res = _ops.Pop().CalcPartOne();
+                            _ops.Peek().Nums.Add(res);
+                            break;
+                        default:
+                            _ops.Peek().Nums.Add(long.Parse(chr.ToString()));
+                            break;
+                    }
+                }
+
+                total += _ops.Pop().CalcPartOne();
+            }
+
+            return total.ToString();
+        }
+
+        public override string SolvePartTwo()
+        {
+            var total = 0L;
+            foreach (var line in Input)
+            {
+                _ops.Push(new Operation());
+
+                foreach (var chr in line.Replace(" ", ""))
+                {
+                    switch (chr)
+                    {
+                        case '+':
+                            _ops.Peek().Operators.Add('+');
+                            break;
+                        case '-':
+                            _ops.Peek().Operators.Add('-');
+                            break;
+                        case '*':
+                            _ops.Peek().Operators.Add('*');
+                            break;
+                        case '(':
+                            _ops.Push(new Operation());
+                            break;
+                        case ')':
+                            var res = _ops.Pop().CalcPartTwo();
+                            _ops.Peek().Nums.Add(res);
+                            break;
+                        default:
+                            _ops.Peek().Nums.Add(long.Parse(chr.ToString()));
+                            break;
+                    }
+                }
+
+                total += _ops.Pop().CalcPartTwo();
+            }
+
+            return total.ToString();
+        }
+
+        private class Operation
+        {
+            public readonly List<long> Nums = new();
+            public readonly List<char> Operators = new(){'+'};
+
+            public long CalcPartOne()
+            {
+                var total = 0L;
+                for (int i = 0; i < Nums.Count; i++)
+                {
+                    if (Operators[i] == '+')
+                        total += Nums[i];
+                    else
+                        total *= Nums[i];
+                }
+
+                return total;
+            }
+
+            public long CalcPartTwo()
+            {
+                var addition = 0L;
+                var allAdditions = new List<long>();
+                for (int i = 0; i < Nums.Count; i++)
+                {
+                    if (Operators[i] == '+')
+                        addition += Nums[i];
+                    else
+                    {
+                        allAdditions.Add(addition);
+                        addition = Nums[i];
+                    }
+                }
+                
+                return allAdditions.Aggregate(addition, (acc, num) => acc * num);
+            }
+        }
+    }
+}
---
+++ AoC2020Benchmark/Day18Benchmark.cs
@@ -0,0 +1,16 @@
+﻿using AoC2020.Days.Day18;
+using BenchmarkDotNet.Attributes;
+
+namespace AoC2020Benchmark
+{
+    public class Day18Benchmark : BaseDayBenchmark
+    {
+        private readonly Day18 _problem = new();
+
+        [Benchmark]
+        public override string Part1() => _problem.SolvePartOne();
+
+        [Benchmark]
+        public override string Part2() => _problem.SolvePartTwo();
+    }
+}
